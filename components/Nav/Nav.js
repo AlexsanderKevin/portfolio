8294477434b9e89e9cb93988688d332b768a6caf
KEVIN ALEXSANDER DE JESUS <kevin13348@gmail.com>
2023-04-05 21:46:32 -0300

    header nav

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,7 +1,14 @@
+import Link from 'next/link'
 import styles from './Nav.module.css'
 
 export default function Nav () {
     return (
-        <nav></nav>
+        <nav className={ styles.navContainer }>
+            <Link className='active' href='#about' > About </Link>
+            <Link href='#skills' > Skills </Link>
+            <Link href='#experience' > Experience </Link>
+            <Link href='#work' > Work </Link>
+            <Link href='#contact' > Contact </Link>
+        </nav>
     )
 }
