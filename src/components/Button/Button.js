368de6151f06a96186413c569d36fe43548e4103
KEVIN ALEXSANDER DE JESUS <kevin13348@gmail.com>
2023-04-14 20:34:19 -0300

    created 'fill-bg' animation

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,7 +3,7 @@ import styles from './Button.module.css'
 const Button = ({ children }) => {
   return (
     <button
-      className={ styles.button }
+      className={` ${styles.button} fill-bg `}
     >{ children }</button>
   )
 }
