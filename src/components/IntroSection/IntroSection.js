912dee7d5ca309bcbcb12db50641991dd83da0b4
kevin <kevin13348@gmail.com>
2023-04-19 14:43:07 -0300

    downloading resume pdf

--- src/components/IntroSection/IntroSection.js
+++ src/components/IntroSection/IntroSection.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './IntroSection.module.css'
+import Resume from '../../assets/Resume.pdf'
 
 const IntroSection = () => {
   return (
@@ -15,7 +16,13 @@ const IntroSection = () => {
       <h1 className='title-main title'>Kevin Alexsander</h1>
       <h2 className='title-secondary title breathing'>I do pretty stuff on the computer</h2>
       <p className={` ${styles.mainParagraph} `}>I'm a software developer specializing in <strong>building and designing front-end</strong> (and ocasionally back-end) applications.</p>
-      <button className='button-main fill-bg'>Resume</button>
+      <a 
+        className='button-main fill-bg' 
+        href={Resume}
+        download='Resume'
+      >
+        Resume
+      </a>
     </section>
   )
 }
