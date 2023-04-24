f3dba380b06e73f777348e3897aa767531e0e443
KEVIN ALEXSANDER DE JESUS <1050482123033@fatec.coorp>
2023-04-24 20:12:08 -0300

    changed skill grid due to responsivity

--- src/components/SkillSection/SkillSection.js
+++ src/components/SkillSection/SkillSection.js
@@ -2,58 +2,40 @@ import React, { useState } from 'react'
 import SectionHeader from '../SectionHeader/SectionHeader'
 import styles from './SkillSection.module.css'
 
-const skills = [
-  {name: 'JavaScript', sub: 'js'},
-  {name: 'Node', sub: 'js'},
-  {name: 'Express', sub: 'js'},
-  {name: 'CSS', sub: 'css'},
-  {name: 'Bootstrap', sub: 'css'},
-  {name: 'React', sub: 'js'},
-  {name: 'Sequelize', sub: 'js'},
-  {name: 'NPM', sub: 'js'},
-  {name: 'SASS', sub: 'css'},
-  {name: 'Tailwind', sub: 'css'},
-  {name: 'JQuery', sub: 'js'},
-  {name: 'TypeScript', sub: 'js'},
-  {name: 'Next', sub: 'js'},
-  {name: 'GIT', sub: ''},
-  {name: 'HTML', sub: ''},
-]
+const skills = {
+  javascript: [ 'JavaScript', 'Node', 'Express', 'React', 'Sequelize', 'NPM', 'JQuery', 'TypeScript', 'Next' ],
+  css: [ 'CSS', 'Bootstrap', 'SASS', 'Tailwind' ],
+  other: [ 'html', 'css' ]
+}
 
 const SkillSection = () => {
-  const [ activeJs, setActiveJs ] = useState(true)
-  const [ activeCss, setActiveCss ] = useState(true)
-
   return (
     <section id='skills' className={`container section-container fade-in-left ${styles.skillSection}`}>
       <SectionHeader position='center'>My Skills</SectionHeader>
 
       <div className={styles.skillContainer}>
         <div className={`${styles.skillGrid}`}>
-          <div 
-            onClick={() => setActiveJs(false)}
-            className={`
-              ${styles.skill} 
-              ${styles.javascript}
-              ${activeJs ? styles.active : ''}
-            `}
-          >JavaScript</div>
 
-          <div 
-            onClick={() => setActiveCss(false)}
-            className={`
-              ${styles.skill} 
-              ${styles.css}
-              ${activeCss ? styles.active : ''}
-            `}
-          >CSS</div>
+          <div className={`${styles.javascriptGrid}`}>
+            {skills.javascript.map( skill => {
+              return(
+                <div className={`${styles.skill}`} key={skill}>{ skill }</div>
+              )
+            })}
+          </div>
 
-          {skills.map(skill => {
-              switch (skill.sub) {
-                case 'js': return !activeJs ? <div className={styles.skill} key={skill.name} data-sub={skill.sub}>{skill.name}</div> : null
-                case 'css': return !activeCss ? <div className={styles.skill} key={skill.name} data-sub={skill.sub}>{skill.name}</div> : null
-                default: return  <div className={styles.skill} key={skill.name} data-sub={skill.sub}>{skill.name}</div>
-              }
+          <div className={`${styles.cssGrid}`}>
+            {skills.css.map( skill => {
+              return(
+                <div className={`${styles.skill}`} key={skill}>{ skill }</div>
+              )
+            })}
+          </div>
+
+          {skills.other.map( skill => {
+            return(
+              <div className={`${styles.skill}`} key={skill}>{ skill }</div>
+            )
           })}
         </div>
       </div>
