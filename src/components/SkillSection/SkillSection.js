1200581e7339b8220461fa6057255e4e4cb4ce74
kevin <kevin13348@gmail.com>
2023-04-24 15:37:43 -0300

    started the skill grid

--- src/components/SkillSection/SkillSection.js
+++ src/components/SkillSection/SkillSection.js
@@ -1,10 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SectionHeader from '../SectionHeader/SectionHeader'
+import styles from './SkillSection.module.css'
+
+const skills = [
+  {name: 'JavaScript', sub: 'js'},
+  {name: 'Node', sub: 'js'},
+  {name: 'Express', sub: 'js'},
+  {name: 'CSS', sub: 'css'},
+  {name: 'Bootstrap', sub: 'css'},
+  {name: 'React', sub: 'js'},
+  {name: 'Sequelize', sub: 'js'},
+  {name: 'NPM', sub: 'js'},
+  {name: 'SASS', sub: 'css'},
+  {name: 'Tailwind', sub: 'css'},
+  {name: 'JQuery', sub: 'js'},
+  {name: 'TypeScript', sub: 'js'},
+  {name: 'Next', sub: 'js'},
+  {name: 'GIT', sub: ''},
+  {name: 'HTML', sub: ''},
+]
 
 const SkillSection = () => {
+  const [ activeJs, setActiveJs ] = useState(true)
+  const [ activeCss, setActiveCss ] = useState(true)
+
   return (
-    <section className='container section-container fade-in-left'>
+    <section id='skills' className={`container section-container fade-in-left ${styles.skillSection}`}>
       <SectionHeader position='center'>My Skills</SectionHeader>
+
+      <div className={styles.skillContainer}>
+        <div className={`${styles.skillGrid}`}>
+          <div 
+            onClick={() => setActiveJs(false)}
+            className={`
+              ${styles.skill} 
+              ${styles.javascript}
+              ${activeJs ? styles.active : ''}
+            `}
+          >JavaScript</div>
+
+          <div 
+            onClick={() => setActiveCss(false)}
+            className={`
+              ${styles.skill} 
+              ${styles.css}
+              ${activeCss ? styles.active : ''}
+            `}
+          >CSS</div>
+
+          {skills.map(skill => {
+              switch (skill.sub) {
+                case 'js': return !activeJs ? <div className={styles.skill} key={skill.name} data-sub={skill.sub}>{skill.name}</div> : null
+                case 'css': return !activeCss ? <div className={styles.skill} key={skill.name} data-sub={skill.sub}>{skill.name}</div> : null
+                default: return  <div className={styles.skill} key={skill.name} data-sub={skill.sub}>{skill.name}</div>
+              }
+          })}
+        </div>
+      </div>
     </section>
   )
 }
