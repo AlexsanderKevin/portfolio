5b9ff3ff101ad2b30f88a8d996e6fff8af2899df
kevin <kevin13348@gmail.com>
2023-04-25 10:07:45 -0300

    animated skill grid

--- src/components/SkillSection/SkillSection.js
+++ src/components/SkillSection/SkillSection.js
@@ -5,10 +5,13 @@ import styles from './SkillSection.module.css'
 const skills = {
   javascript: [ 'JavaScript', 'Node', 'Express', 'React', 'Sequelize', 'NPM', 'JQuery', 'TypeScript', 'Next' ],
   css: [ 'CSS', 'Bootstrap', 'SASS', 'Tailwind' ],
-  other: [ 'html', 'css' ]
+  other: [ 'html', 'GIT' ]
 }
 
 const SkillSection = () => {
+  const [ activeJs, setActiveJs ] = useState(true)
+  const [ activeCss, setActiveCss ] = useState(true)
+
   return (
     <section id='skills' className={`container section-container fade-in-left ${styles.skillSection}`}>
       <SectionHeader position='center'>My Skills</SectionHeader>
@@ -16,25 +19,29 @@ const SkillSection = () => {
       <div className={styles.skillContainer}>
         <div className={`${styles.skillGrid}`}>
 
-          <div className={`${styles.javascriptGrid}`}>
-            {skills.javascript.map( skill => {
-              return(
-                <div className={`${styles.skill}`} key={skill}>{ skill }</div>
-              )
-            })}
+          <div className={`${styles.javascriptGrid} ${activeJs ? styles.active : ''}`}>
+            {skills.javascript.map( (skill, index) => (
+              <div 
+                onClick={index === 0 ? () => setActiveJs(!activeJs) : null}
+                className={`${styles.skill}`} 
+                key={skill}
+              >{ skill }</div>
+            ))}
           </div>
 
-          <div className={`${styles.cssGrid}`}>
-            {skills.css.map( skill => {
-              return(
-                <div className={`${styles.skill}`} key={skill}>{ skill }</div>
-              )
-            })}
+          <div className={`${styles.cssGrid} ${activeCss ? styles.active : ''}`}>
+            {skills.css.map( (skill, index) => (
+              <div 
+                onClick={index === 0 ? () => setActiveCss(!activeCss) : null}
+                className={`${styles.skill}`} 
+                key={skill}
+              >{ skill }</div>
+            ))}
           </div>
 
           {skills.other.map( skill => {
             return(
-              <div className={`${styles.skill}`} key={skill}>{ skill }</div>
+              <div className={`${styles.skill}`} style={{backgroundColor: '#FF8058'}} key={skill}>{ skill }</div>
             )
           })}
         </div>
