53da2432d85dad9fcbbc5175f31223ec56d13037
kevin <kevin13348@gmail.com>
2023-04-27 20:17:47 -0300

    finished the javascript grid

--- src/components/SkillSection/SkillSection.js
+++ src/components/SkillSection/SkillSection.js
@@ -1,8 +1,18 @@
 import React, { useState } from 'react'
 import SectionHeader from '../SectionHeader/SectionHeader'
 import styles from './SkillSection.module.css'
+import skills from './Skills'
+import { ReactComponent as IconJavascript } from '../../assets/svg/icon-javascript.svg'
+import { ReactComponent as IconNode } from '../../assets/svg/icon-node.svg'
+import { ReactComponent as IconExpress } from '../../assets/svg/icon-express.svg'
+import { ReactComponent as IconReact } from '../../assets/svg/icon-react.svg'
+import { ReactComponent as IconSequelize } from '../../assets/svg/icon-sequelize.svg'
+import { ReactComponent as IconNpm } from '../../assets/svg/icon-npm.svg'
+import { ReactComponent as IconJquery } from '../../assets/svg/icon-jquery.svg'
+import { ReactComponent as IconTypescript } from '../../assets/svg/icon-typescript.svg'
+import { ReactComponent as IconNext } from '../../assets/svg/icon-next.svg'
 
-const skills = {
+const _skills = {
   javascript: [ 'JavaScript', 'Node', 'Express', 'React', 'Sequelize', 'NPM', 'JQuery', 'TypeScript', 'Next' ],
   css: [ 'CSS', 'Bootstrap', 'SASS', 'Tailwind' ],
   other: [ 'html', 'GIT' ]
@@ -20,30 +30,32 @@ const SkillSection = () => {
         <div className={`${styles.skillGrid}`}>
 
           <div className={`${styles.javascriptGrid} ${activeJs ? styles.active : ''}`}>
-            {skills.javascript.map( (skill, index) => (
-              <div 
-                onClick={index === 0 ? () => setActiveJs(!activeJs) : null}
-                className={`${styles.skill}`} 
-                key={skill}
-              >{ skill }</div>
-            ))}
+            <div className='skill'><IconJavascript height={'6rem'} width={'6rem'}/></div>
+            <div className='skill'><IconNode height={'6rem'} width={'6rem'}/></div>
+            <div className='skill'><IconExpress height={'6rem'} width={'6rem'}/></div>
+            <div className='skill'><IconReact height={'6rem'} width={'6rem'}/></div>
+            <div className='skill'><IconSequelize height={'6rem'} width={'6rem'}/></div>
+            <div className='skill'><IconNpm height={'6rem'} width={'6rem'}/></div>
+            <div className='skill'><IconJquery height={'6rem'} width={'6rem'}/></div>
+            <div className='skill'><IconTypescript height={'6rem'} width={'6rem'}/></div>
+            <div className='skill'><IconNext height={'6rem'} width={'6rem'}/></div>
           </div>
 
           <div className={`${styles.cssGrid} ${activeCss ? styles.active : ''}`}>
-            {skills.css.map( (skill, index) => (
+            {/* {skills.css.map( (skill, index) => (
               <div 
                 onClick={index === 0 ? () => setActiveCss(!activeCss) : null}
                 className={`${styles.skill}`} 
                 key={skill}
               >{ skill }</div>
-            ))}
+            ))} */}
           </div>
 
-          {skills.other.map( skill => {
+          {/* {skills.other.map( skill => {
             return(
               <div className={`${styles.skill}`} style={{backgroundColor: '#FF8058'}} key={skill}>{ skill }</div>
             )
-          })}
+          })} */}
         </div>
       </div>
     </section>
---
+++ src/components/SkillSection/Skills.js
@@ -0,0 +1,10 @@
+const FOLDER_PATH = '../../assets/svg/'
+
+const skills = {
+  javascript: [
+    { name: 'JavaScript', img: FOLDER_PATH + 'icon-javascript.svg' },
+    { name: 'Css', img: FOLDER_PATH + 'icon-css.svg' },
+  ]
+}
+
+export default skills
