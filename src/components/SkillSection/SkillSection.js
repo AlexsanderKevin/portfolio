a2752241d8e0cf464211fb84f63dcf7c74892c9a
kevin <kevin13348@gmail.com>
2023-05-08 09:32:44 -0300

    added planet svgs

--- src/components/SkillSection/SkillSection.js
+++ src/components/SkillSection/SkillSection.js
@@ -1,21 +1,10 @@
 import React, { useState } from 'react'
 import SectionHeader from '../SectionHeader/SectionHeader'
 import styles from './SkillSection.module.css'
-import { ReactComponent as IconJavascript } from '../../assets/svg/icons/icon-javascript.svg'
-import { ReactComponent as IconNode } from '../../assets/svg/icons/icon-node.svg'
-import { ReactComponent as IconExpress } from '../../assets/svg/icons/icon-express.svg'
-import { ReactComponent as IconReact } from '../../assets/svg/icons/icon-react.svg'
-import { ReactComponent as IconSequelize } from '../../assets/svg/icons/icon-sequelize.svg'
-import { ReactComponent as IconNpm } from '../../assets/svg/icons/icon-npm.svg'
-import { ReactComponent as IconJquery } from '../../assets/svg/icons/icon-jquery.svg'
-import { ReactComponent as IconTypescript } from '../../assets/svg/icons/icon-typescript.svg'
-import { ReactComponent as IconNext } from '../../assets/svg/icons/icon-next.svg'
-import { ReactComponent as IconCss } from '../../assets/svg/icons/icon-css.svg'
-import { ReactComponent as IconBootstrap } from '../../assets/svg/icons/icon-bootstrap.svg'
-import { ReactComponent as IconSass } from '../../assets/svg/icons/icon-sass.svg'
-import { ReactComponent as IconTailwind } from '../../assets/svg/icons/icon-tailwind.svg'
-import { ReactComponent as IconHtml } from '../../assets/svg/icons/icon-html.svg'
-import { ReactComponent as IconGit } from '../../assets/svg/icons/icon-git.svg'
+import { ReactComponent as PlanetHtml } from '../../assets/svg/planets/planet-html.svg'
+import { ReactComponent as PlanetGit } from '../../assets/svg/planets/planet-git.svg'
+import { ReactComponent as SystemJavascrip } from '../../assets/svg/planets/system-javascript.svg'
+import { ReactComponent as SystemCss } from '../../assets/svg/planets/system-css.svg'
 
 const SkillSection = () => {
   const [ activeJs, setActiveJs ] = useState(false)
@@ -23,35 +12,10 @@ const SkillSection = () => {
 
   return (
     <section id='skills' className={`container section-container fade-in-left ${styles.skillSection}`}>
-      <SectionHeader position='center'>My Skills</SectionHeader>
-
-      <div className={styles.skillContainer}>
-        <div className={`${styles.skillGrid}`}>
-
-          <div className={`${styles.javascriptGrid} ${activeJs ? styles.active : ''}`}>
-            <div className={styles.skill}><IconJavascript height={'4.5rem'} width={'5rem'}/></div>
-            <div className={styles.skill}><IconNode height={'5rem'} width={'5rem'}/></div>
-            <div className={styles.skill}><IconExpress height={'4.8rem'} width={'6rem'}/></div>
-            <div className={styles.skill}><IconReact height={'5rem'} width={'6rem'}/></div>
-            <div className={styles.skill}><IconSequelize height={'5rem'} width={'6rem'}/></div>
-            <div className={styles.skill}><IconNpm height={'4.2rem'} width={'6rem'}/></div>
-            <div className={styles.skill}><IconJquery height={'4.5rem'} width={'6rem'}/></div>
-            <div className={styles.skill}><IconTypescript height={'4.2rem'} width={'6rem'}/></div>
-            <div className={styles.skill}><IconNext height={'4.8rem'} width={'6rem'}/></div>
-          </div>
-
-          <div className={`${styles.cssGrid} ${activeCss ? styles.active : ''}`}>
-            <div className={styles.skill}><IconCss height={'4.5rem'} width={'6rem'}/></div>
-            <div className={styles.skill}><IconBootstrap height={'4rem'} width={'6rem'}/></div>
-            <div className={styles.skill}><IconSass height={'4.8rem'} width={'6rem'}/></div>
-            <div className={styles.skill}><IconTailwind height={'4.5rem'} width={'5rem'}/></div>
-          </div>
-
-          <div className={styles.skill}><IconHtml height={'4.5rem'} width={'6rem'}/></div>
-          <div className={styles.skill}><IconGit height={'4.8rem'} width={'6rem'}/></div>
-
-        </div>
-      </div>
+      <PlanetHtml className={styles.planetHtml}/>
+      <PlanetGit className={styles.planetGit}/>
+      <SystemJavascrip className={styles.systemJavascrip}/>
+      <SystemCss className={styles.systemCss}/>
     </section>
   )
 }
