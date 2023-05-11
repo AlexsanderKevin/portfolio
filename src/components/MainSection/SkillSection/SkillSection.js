5b793fee3bc2e00055daea2590344c8d856c911a
kevin <kevin13348@gmail.com>
2023-05-11 13:55:06 -0300

    skill section activating accourding to the context

--- src/components/MainSection/SkillSection/SkillSection.js
+++ src/components/MainSection/SkillSection/SkillSection.js
@@ -1,13 +1,22 @@
-import React, { useState } from 'react'
+import React, { useCallback, useContext, useState } from 'react'
 import styles from './SkillSection.module.css'
 import { ReactComponent as PlanetHtml } from '../../../assets/svg/planets/planet-html.svg'
 import { ReactComponent as PlanetGit } from '../../../assets/svg/planets/planet-git.svg'
 import { ReactComponent as SystemJavascrip } from '../../../assets/svg/planets/system-javascript.svg'
 import { ReactComponent as SystemCss } from '../../../assets/svg/planets/system-css.svg'
+import { GlobalContext } from '../../../GlobalContext'
 
 const SkillSection = () => {
+  const { section } = useContext(GlobalContext)
+
   return (
-    <section id='skills' className={`container section-container fade-in-left ${styles.skillSection}`}>
+    <section id='skills' className={`
+      container 
+      section-container 
+      fade-in-left 
+      ${styles.skillSection}
+      ${section === 1 ? styles.active : ''}
+    `}>
       <PlanetHtml className={styles.planetHtml}/>
       <PlanetGit className={styles.planetGit}/>
       <SystemJavascrip className={styles.systemJavascrip}/>
