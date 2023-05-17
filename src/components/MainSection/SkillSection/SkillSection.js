a41e6f9dc277af134bb7c71dcb4361528b1d2d89
kevin <kevin13348@gmail.com>
2023-05-17 10:05:21 -0300

    added all svg icons

--- src/components/MainSection/SkillSection/SkillSection.js
+++ src/components/MainSection/SkillSection/SkillSection.js
@@ -1,12 +1,16 @@
 import React, { useContext } from 'react'
 import styles from './SkillSection.module.css'
 import { GlobalContext } from '../../../GlobalContext'
-import iconHtml from '../../../assets/img/icons/icon-html.png'
-import iconCss from '../../../assets/img/icons/icon-css.png'
-import iconGit from '../../../assets/img/icons/icon-git.png'
-import iconReact from '../../../assets/img/icons/icon-react.png'
 import { ReactComponent as JavascriptSVG } from '../../../assets/svg/icons/svg-javascript.svg'
 import { ReactComponent as NodetSVG } from '../../../assets/svg/icons/svg-node.svg'
+import { ReactComponent as HtmlSVG } from '../../../assets/svg/icons/svg-html.svg'
+import { ReactComponent as CssSVG } from '../../../assets/svg/icons/svg-css.svg'
+import { ReactComponent as GitSVG } from '../../../assets/svg/icons/svg-git.svg'
+import { ReactComponent as ReactSVG } from '../../../assets/svg/icons/svg-react.svg'
+import { ReactComponent as JquerySVG } from '../../../assets/svg/icons/svg-jquery.svg'
+import { ReactComponent as BootstrapSVG } from '../../../assets/svg/icons/svg-bootstrap.svg'
+import { ReactComponent as SassSVG } from '../../../assets/svg/icons/svg-sass.svg'
+import { ReactComponent as FigmaSVG } from '../../../assets/svg/icons/svg-figma.svg'
 
 const SkillSection = () => {
   const { section } = useContext(GlobalContext)
@@ -22,20 +26,20 @@ const SkillSection = () => {
     >
       <div className={styles.planetarySystem}>
         <div className={`${styles.orbit} ${styles.orbit3}`}>
-          <div className={`glass-card ${styles.planet}`}><img src={iconReact} alt=''></img></div>
-          <div className={`glass-card ${styles.planet}`}></div>
-          <div className={`glass-card ${styles.planet}`}></div>
-          <div className={`glass-card ${styles.planet}`}></div>
+          <div className={`glass-card ${styles.planet}`}><ReactSVG/></div>
+          <div className={`glass-card ${styles.planet}`}><JquerySVG/></div>
+          <div className={`glass-card ${styles.planet}`}><BootstrapSVG/></div>
+          <div className={`glass-card ${styles.planet}`}><SassSVG/></div>
 
           <div className={`${styles.orbit} ${styles.orbit2}`}>
-            <div className={`glass-card ${styles.planet}`}><img src={iconGit} alt=''></img></div>
-            <div className={`glass-card ${styles.planet}`}></div>
+            <div className={`glass-card ${styles.planet}`}><GitSVG/></div>
+            <div className={`glass-card ${styles.planet}`}><FigmaSVG/></div>
 
             <div className={`${styles.orbit} ${styles.orbit1}`}>
               <div className={`glass-card ${styles.planet}`}><JavascriptSVG/></div>
               <div className={`glass-card ${styles.planet}`}><NodetSVG/></div>
-              <div className={`glass-card ${styles.planet}`}><img src={iconHtml} alt=''></img></div>
-              <div className={`glass-card ${styles.planet}`}><img src={iconCss} alt=''></img></div>
+              <div className={`glass-card ${styles.planet}`}><HtmlSVG/></div>
+              <div className={`glass-card ${styles.planet}`}><CssSVG/></div>
 
               <div className={`glass-card ${styles.planetCenter}`}></div>
             </div>
