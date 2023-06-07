d2411c992c598cd7498922e206dcbeadc3c328ad
kevin <kevin13348@gmail.com>
2023-06-07 10:06:38 -0300

    added bust mini to the about card

--- src/components/MainSection/AboutSection/InfoCard/AboutInfoCard.js
+++ src/components/MainSection/AboutSection/InfoCard/AboutInfoCard.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import styles from './SkillInfoCard.module.css'
+import aboutStyles from './AboutInfoCard.module.css'
+import BustMini from '../../../Ilustrations/BustMini/BustMini'
 
 const AboutInfoCard = ({ active }) => {
 
   if (!active) return null
   return (
     <div id='skill-infocard-overlay' className={styles.background}>
-      <div className={`glass-card ${styles.infoCard}`}>
-        <div className={styles.cardHeader}>Who am I</div>
+      <div className={`glass-card ${styles.infoCard} ${aboutStyles.aboutInfoCard}`}>
+        <div className={styles.cardHeader}>
+          <h1 className={`title`}>Who am I</h1>
+        </div>
+        <div className={`${styles.cardBody} ${styles.aboutCardBody}`}>
+          <BustMini active={true}/>
+        </div>
       </div>
     </div>
   )
