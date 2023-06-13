686c91916aeaffba2f49a26d0ab2dbc4815f663b
kevin <kevin13348@gmail.com>
2023-06-13 09:24:29 -0300

    added some info at the card

--- src/components/MainSection/AboutSection/InfoCard/AboutInfoCard.js
+++ src/components/MainSection/AboutSection/InfoCard/AboutInfoCard.js
@@ -12,8 +12,19 @@ const AboutInfoCard = ({ active }) => {
         <div className={styles.cardHeader}>
           <h1 className={`title`}>Who am I</h1>
         </div>
-        <div className={`${styles.cardBody} ${styles.aboutCardBody}`}>
-          <BustMini active={true}/>
+        <div className={`${aboutStyles.aboutCardBody}`}>
+          <BustMini active={true} height='170' width='150'/>
+          <ul className={`${aboutStyles.aboutDetails}`}>
+            <li>09/03/2002</li>
+            <li>Brazil</li>
+            <li>Front-end</li>
+          </ul>
+          <section className={`${aboutStyles.bio}`}>
+            <h2>Kevin Alexsander de Jesus</h2>
+            <p>I work with software development since May 2022 and I am passionate about technology and design and always seek to improve my skills. </p>
+            <p>I believe that the combination of technical knowledge and design skills is fundamental to build high-quality applications.</p>
+            <p>I am excited to apply these skills to a successful career in front-end. Contact me to exchange ideas about technology and programming!</p>
+          </section>
         </div>
       </div>
     </div>
